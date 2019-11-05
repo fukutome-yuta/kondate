6389b53e85a18d3643728fc51ce3fd250868a724
fukutome-yuta <a.zero.yu@gmail.com>
2019-11-05 12:51:41 +0900

    献立編集画面更新

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -27,14 +27,26 @@ class MenusController < ApplicationController
     start_date = Date.parse(params[:start_date])
     end_date = Date.parse(params[:end_date])
 
+    menus = []
+
     (start_date..end_date).each do |date|
-      menu = current_user.menus.new(schedule: date)
-menu.save!
+      #menu = current_user.menus.new(schedule: date)
+      menus << current_user.menus.new(schedule: date)
+      #menu.save!
+    end
+    current_user.menus.import menus
       # unless menu.save
       #   redirect_to new_menu_url, notice: "献立表の作成に失敗しました。" and return
       # end 
-    end
-    redirect_to menus_edit_url, notice: "献立表を作成しました。" and return
+    redirect_to menus_edit_url, notice: "献立表を作成しました。"
+  end
+
+  def update
+    before = params[:before_id]
+    after = params[:after_id]
+    logger.debug before
+    logger.debug after
+    redirect_to menus_edit_url
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
 
   root to: 'recipes#index'
   resources :recipes
-  resources :menus, only: [:index, :new, :create, :update]
+  resources :menus, only: [:index, :new, :create]
   get '/menus/edit', to: 'menus#edit'
   get '/menus/select/:id', to: 'menus#select', as: 'select'
+  post '/menus/update', to: 'menus#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
